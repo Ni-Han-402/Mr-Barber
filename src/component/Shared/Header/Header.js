51ea7d1acb7c38c7c51e417ff2a0a0d2bb4e0c09
Nihan Zyan <nihanzyan38@gmail.com>
2022-04-17 15:32:10 +0600

    Header Added

---
+++ src/component/Shared/Header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../images/logo.png'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <>
+            <div className="header">
+                <div className="container">
+                <div className="logo">
+                            <img src={logo} alt="barber-logo" />
+                        </div>
+                    <div className="navigation">
+                        <div className="menu">
+                            <Link className='menu-item' to={'/home'}>HOME</Link>
+                            <Link className='menu-item' to={'/Checkout'}>CHECKOUT</Link>
+                            <Link className='menu-item' to={'/blog'}>BLOGS</Link>
+                            <Link className='menu-item' to={'/about'}>ABOUT</Link>
+                        </div>
+                      
+                        <div className="login-container">
+                            <Link to={'/login'}><button className='btn login-btn'>LOGIN</button></Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
