dce56537b9d45aa6adddfe62f8230ab6aa499acb
Nihan Zyan <nihanzyan38@gmail.com>
2022-04-18 21:19:49 +0600

    About page Added

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import aboutImage from '../../images/nihan.jpg'
+import './About.css'
 
 const About = () => {
     return (
         <div>
-            <h1>This is About</h1>
+            <div className="container">
+                <div className="about-container">
+                    <div className="about-img">
+                        <img src={aboutImage} alt="" />
+                    </div>
+                    <div className="about-content">
+                        <h2>Nihan Zayn</h2>
+                        <p>Hi, I'm A Professional Barber. If You Want To Cut Your Hair, Style Your Beard And Wash Your Hair With Shampoo Then Contact With Me.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
